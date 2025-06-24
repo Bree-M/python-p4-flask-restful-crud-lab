abf7f396c872488f823e58d7d9f8b9fdcb342b4c
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-06-24 06:57:53 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -11,14 +11,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+
 migrate = Migrate(app, db)
 db.init_app(app)
+
 
 api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -38,16 +39,29 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
-
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get_or_404(id)
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
+
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get_or_404(id)
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
